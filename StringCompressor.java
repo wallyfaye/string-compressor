dea8deb76ddd84798899ce20289f6c7dd96c0f43
Wally Faye <wally.faye@gmail.com>
2017-07-13 23:18:45 -0400

    private method

--- StringCompressor.java
+++ StringCompressor.java
@@ -2,12 +2,18 @@ public class StringCompressor{
 
 	public String res;
 
+	private String update_string(String main_string, char letter, int letter_count){
+		main_string += letter;
+		main_string += letter_count;
+		return main_string;
+	}
+
 	public StringCompressor(String arg_1){
-		res = arg_1;
-		System.out.println(arg_1);
+
 		String new_string = "";
 		char letter_buffer = '\0';
 		int last_char_int = 0;
+
 		for(int i = 0; i < arg_1.length(); i++){
 			char this_char = arg_1.charAt(i);
 			if(letter_buffer == '\0'){
@@ -16,17 +22,18 @@ public class StringCompressor{
 			} else if (this_char == letter_buffer) {
 				last_char_int++;
 			} else {
-				new_string += letter_buffer;
-				new_string += last_char_int;
+				new_string = this.update_string(new_string, letter_buffer, last_char_int);
 				letter_buffer = this_char;
 				last_char_int = 1;
 			}
 		}
-		new_string += letter_buffer;
-		new_string += last_char_int;
-		System.out.println(new_string);
+
+		new_string = this.update_string(new_string, letter_buffer, last_char_int);
+
 		if(new_string.length() < arg_1.length()){
 			res = new_string;
+		} else {
+			res = arg_1;
 		}
 	}
 
